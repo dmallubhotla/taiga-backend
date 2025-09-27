28c411e6e564427be5fd777f138c2a545e1f2da2
Deepak Mallubhotla <dmallubhotla+github@gmail.com>
2025-09-27 11:49:46 -0500

    adding gopls

--- flake.nix
+++ flake.nix
@@ -3,17 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    gomod2nix = {
+      url = "github:nix-community/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { nixpkgs, ... }@inputs:
+    { nixpkgs, gomod2nix, ... }@inputs:
     let
       supportedSystems = [ "x86_64-linux" ];
       pkgsFor =
         system:
         nixpkgs.legacyPackages.${system}.extend (
-          nixpkgs.lib.composeManyExtensions ([ ])
+          nixpkgs.lib.composeManyExtensions [ gomod2nix.overlays.default ]
           # nixpkgs.lib.composeManyExtensions ([ ] ++ builtins.attrValues self.overlays)
         );
       eachSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f (pkgsFor system));
@@ -28,7 +35,14 @@
 
       devShells = eachSystem (pkgs: {
         default = pkgs.mkShell {
-          packages = [ pkgs.just ];
+          packages = [
+            pkgs.just
+            pkgs.gomod2nix
+            pkgs.go
+            pkgs.gopls
+            pkgs.gotools
+            pkgs.go-tools
+          ];
 
           # Will be executed before entering the shell
           # or running a command
